5a809973e1e5347843b70d4a04b03c37d4738599
Muzz <creative.joomdev@gmail.com>
2021-06-17 10:03:06 +0500

    build files

---
+++ build/static/home.046980efe356fcc3d5bf.hot-update.js
@@ -0,0 +1,365 @@
+webpackHotUpdate("home",{
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/Add.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/ArrowBack.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/ArrowDown.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/ArrowForward.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/ArrowLeft.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/ArrowRight.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/ArrowUp.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/ArrowUpDown.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/AtSign.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/Attachment.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/Bell.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/Calendar.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/Chat.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/Check.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/CheckCircle.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/ChevronDown.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/ChevronLeft.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/ChevronRight.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/ChevronUp.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/Close.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/Copy.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/Delete.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/Download.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/DragHandle.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/Edit.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/Email.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/ExternalLink.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/Hamburger.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/Info.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/InfoOutline.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/Link.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/Lock.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/Minus.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/Moon.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/NotAllowed.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/Phone.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/PlusSquare.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/Question.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/QuestionOutline.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/Repeat.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/RepeatClock.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/Search.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/Search2.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/Settings.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/SmallAdd.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/SmallClose.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/Spinner.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/Star.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/Sun.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/Time.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/TriangleDown.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/TriangleUp.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/Unlock.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/UpDown.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/View.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/ViewOff.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/Warning.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/WarningTwo.js":
+false,
+
+/***/ "./node_modules/@chakra-ui/icons/dist/esm/index.js":
+false,
+
+/***/ "./node_modules/@frontity/components/switch.tsx":
+false,
+
+/***/ "./node_modules/bootstrap/dist/css/bootstrap.min.css":
+false,
+
+/***/ "./node_modules/react-icons/io/index.esm.js":
+false,
+
+/***/ "./node_modules/react-icons/lib/esm/iconBase.js":
+false,
+
+/***/ "./node_modules/react-icons/lib/esm/iconContext.js":
+false,
+
+/***/ "./node_modules/react-icons/lib/esm/iconsManifest.js":
+false,
+
+/***/ "./node_modules/react-icons/lib/esm/index.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/assets/pattern-tile-green.svg":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/assets/pattern-tile-light-fade.svg":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/archive/index.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/blog/agent.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/faqsComp/Accordians.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/footer.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/header/About.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/header/Faqs.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/header/SearchBar.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/header/header.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/header/index.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/header/loginAndReg.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/header/navigation.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/header/social-menu.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/hooks/useScrollProgress.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/hooks/useSearch.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/index.js":
+/*!****************************************************************!*\
+  !*** ./packages/frontity-chakra-theme/src/components/index.js ***!
+  \****************************************************************/
+/*! exports provided: default */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: E:\\\\Projects\\\\billahome-frontity\\\\packages\\\\frontity-chakra-theme\\\\src\\\\components\\\\index.js: Unexpected token (17:0)\\n\\n\\u001b[0m \\u001b[90m 15 |\\u001b[39m \\u001b[36mimport\\u001b[39m \\u001b[33mFaqs\\u001b[39m \\u001b[36mfrom\\u001b[39m \\u001b[32m\\\"./header/Faqs\\\"\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 16 |\\u001b[39m \\u001b[36mimport\\u001b[39m \\u001b[33mAgent\\u001b[39m \\u001b[36mfrom\\u001b[39m \\u001b[32m\\\"./blog/agent\\\"\\u001b[39m\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 17 |\\u001b[39m \\u001b[33m<<\\u001b[39m\\u001b[33m<<\\u001b[39m\\u001b[33m<<\\u001b[39m\\u001b[33m<\\u001b[39m \\u001b[33mHEAD\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m    |\\u001b[39m \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 18 |\\u001b[39m \\u001b[36mimport\\u001b[39m \\u001b[33mSearch\\u001b[39m \\u001b[36mfrom\\u001b[39m \\u001b[32m\\\"./serch-result/search\\\"\\u001b[39m\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 19 |\\u001b[39m \\u001b[33m===\\u001b[39m\\u001b[33m===\\u001b[39m\\u001b[33m=\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 20 |\\u001b[39m \\u001b[36mimport\\u001b[39m \\u001b[33mContactComp\\u001b[39m \\u001b[36mfrom\\u001b[39m \\u001b[32m\\\"./header/ContactComp\\\"\\u001b[39m\\u001b[0m\\n    at Object._raise (E:\\\\Projects\\\\billahome-frontity\\\\node_modules\\\\@babel\\\\parser\\\\src\\\\parser\\\\error.js:134:45)\\n    at Object.raiseWithData (E:\\\\Projects\\\\billahome-frontity\\\\node_modules\\\\@babel\\\\parser\\\\src\\\\parser\\\\error.js:129:17)\\n    at Object.raise (E:\\\\Projects\\\\billahome-frontity\\\\node_modules\\\\@babel\\\\parser\\\\src\\\\parser\\\\error.js:78:17)\\n    at Object.unexpected (E:\\\\Projects\\\\billahome-frontity\\\\node_modules\\\\@babel\\\\parser\\\\src\\\\parser\\\\util.js:179:16)\\n    at Object.parseExprAtom (E:\\\\Projects\\\\billahome-frontity\\\\node_modules\\\\@babel\\\\parser\\\\src\\\\parser\\\\expression.js:1213:20)\\n    at Object.parseExprAtom (E:\\\\Projects\\\\billahome-frontity\\\\node_modules\\\\@babel\\\\parser\\\\src\\\\plugins\\\\jsx\\\\index.js:576:22)\\n    at Object.parseExprSubscripts (E:\\\\Projects\\\\billahome-frontity\\\\node_modules\\\\@babel\\\\parser\\\\src\\\\parser\\\\expression.js:616:23)\\n    at Object.parseUpdate (E:\\\\Projects\\\\billahome-frontity\\\\node_modules\\\\@babel\\\\parser\\\\src\\\\parser\\\\expression.js:596:21)\\n    at Object.parseMaybeUnary (E:\\\\Projects\\\\billahome-frontity\\\\node_modules\\\\@babel\\\\parser\\\\src\\\\parser\\\\expression.js:563:23)\\n    at Object.parseExprOps (E:\\\\Projects\\\\billahome-frontity\\\\node_modules\\\\@babel\\\\parser\\\\src\\\\parser\\\\expression.js:367:23)\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuL3BhY2thZ2VzL2Zyb250aXR5LWNoYWtyYS10aGVtZS9zcmMvY29tcG9uZW50cy9pbmRleC5qcy5qcyIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./packages/frontity-chakra-theme/src/components/index.js\n");
+
+/***/ }),
+
+/***/ "./packages/frontity-chakra-theme/src/components/loading.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/menu/index.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/menu/menu-button.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/menu/menu-drawer.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/menu/menu-item.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/newsletter.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/page404.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/post/author-bio.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/post/featured-media.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/post/post-categories.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/post/post-header.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/post/post-progressbar.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/post/post.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/search/index.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/search/search-button.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/search/search-form.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/search/search-modal.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/search/search-results.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/serch-result/search.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/styles/c_section.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/styles/font-face.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/styles/pattern-box.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/styles/section.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/styles/wp-site/base.min.css":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/styles/wp-site/faqs.min.css":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/styles/wp-site/index.css":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/styles/wp-site/main.css":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/styles/wp-site/main.min.css":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/components/title.js":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/fonts/KelsonSans-Bold.ttf":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/fonts/KelsonSans-Bold.woff":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/fonts/poppins-fonts/webfonts/Poppins-Bold.ttf":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/fonts/poppins-fonts/webfonts/Poppins-Bold.woff":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/fonts/tielabs-fonticon/tielabs-fonticon.ttf":
+false,
+
+/***/ "./packages/frontity-chakra-theme/src/fonts/tielabs-fonticon/tielabs-fonticon.woff":
+false
+
+})
