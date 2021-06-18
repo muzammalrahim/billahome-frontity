bbfdb2e080aad0a7106ddc1fe99cb7c2955d41d0
Taimoor Khan <taimoor36372@gmail.com>
2021-06-18 07:48:39 -0700

    buy property page

--- packages/frontity-chakra-theme/src/components/archive/AllPropertiesArchive.js
+++ packages/frontity-chakra-theme/src/components/archive/AllPropertiesArchive.js
@@ -1,53 +1,60 @@
-import React, {useEffect, useState} from "react";
-import { Box, Heading, Button , Center, ButtonGroup, SimpleGrid } from "@chakra-ui/react";
-import List from './archive'
+import React, { useEffect, useState } from "react";
+import {
+  Box,
+  Heading,
+  Button,
+  Center,
+  ButtonGroup,
+  SimpleGrid,
+} from "@chakra-ui/react";
+import List from "./archive";
 import { connect } from "frontity";
-import Propertyview from './property-view'
+import Propertyview from "./property-view";
 import { FeaturedPostSection } from "../featured-post/featured-post";
 import { formatPostData, splitPosts } from "../helpers";
 import AllProperty from "./allProperties";
-import SearchBar from "../header/SearchBar"
+import SearchBar from "../header/SearchBar";
 
-import {ChevronLeftIcon} from "@chakra-ui/icons";
-import {ChevronRightIcon} from "@chakra-ui/icons";
+import { ChevronLeftIcon } from "@chakra-ui/icons";
+import { ChevronRightIcon } from "@chakra-ui/icons";
 
-const AllPropertiesArchive = ({ state, actions, libraries })=>{
-
- const data = state.source.get(state.router.link);
+const AllPropertiesArchive = ({ state, actions, libraries }) => {
+  const data = state.source.get(state.router.link);
 
   const [firstThreePosts, othersPosts] = splitPosts(state, data.items);
 
-  const [currentlyAddedItemsFromState, setcurrentlyAddedItemsFromState] = useState([]);
+  const [currentlyAddedItemsFromState, setcurrentlyAddedItemsFromState] =
+    useState([]);
   const [NavigateToPageNo, setNavigateToPageNo] = useState(1);
-  
 
-  let allProperties = '';
+  let allProperties = "";
 
-  useEffect( async() => {
+  useEffect(async () => {
     await actions.source.fetch(`/properties/${NavigateToPageNo}`);
     allProperties = state.source.get(`/properties/${NavigateToPageNo}`).items;
-    setcurrentlyAddedItemsFromState(allProperties)
-  }, [])
+    setcurrentlyAddedItemsFromState(allProperties);
+  }, []);
 
-
-  const PrevPage = async () => {
-    await actions.source.fetch(`/properties/${NavigateToPageNo}`);
-    allProperties = state.source.get(`/properties/${NavigateToPageNo}`).items;
-    setcurrentlyAddedItemsFromState(allProperties)
-    setNavigateToPageNo(NavigateToPageNo -1)
-  }
+  const PreviousPage = async () => {
+    await actions.source.fetch(`/properties/${NavigateToPageNo - 1}`);
+    allProperties = state.source.get(
+      `/properties/${NavigateToPageNo - 1}`
+    ).items;
+    setcurrentlyAddedItemsFromState(allProperties);
+    setNavigateToPageNo(NavigateToPageNo - 1);
+  };
   const NextPage = async () => {
-    await actions.source.fetch(`/properties/${NavigateToPageNo}`);
-    allProperties = state.source.get(`/properties/${NavigateToPageNo}`).items;
-    setcurrentlyAddedItemsFromState(allProperties)
-    setNavigateToPageNo(NavigateToPageNo +1)
-    
-  }
+    await actions.source.fetch(`/properties/${NavigateToPageNo + 1}`);
+    allProperties = state.source.get(
+      `/properties/${NavigateToPageNo + 1}`
+    ).items;
+    setcurrentlyAddedItemsFromState(allProperties);
+    setNavigateToPageNo(NavigateToPageNo + 1);
+  };
 
-console.log("all properties",currentlyAddedItemsFromState)
-return (
-  <Box bg="accent.50" as="section">
-   <SearchBar/>
+  return (
+    <Box bg="accent.50" as="section">
+      <SearchBar />
       <Box
         py={{ base: "64px", md: "80px" }}
         px={{ base: "24px", md: "40px" }}
@@ -55,38 +62,52 @@ return (
         maxWidth="1200px"
         mx="auto"
       >
-  <SimpleGrid
-  mt={{ base: "64px", md: "80px" }}
-  columns={{ base: 1, md: 3 }}
-  spacing="20px"
->
+        <SimpleGrid
+          mt={{ base: "64px", md: "80px" }}
+          columns={{ base: 1, md: 3 }}
+          spacing="20px"
+        >
+          {currentlyAddedItemsFromState?.map(
+            ({
+              title,
+              link,
+              excerpt,
+              featured_media,
+              _embedded,
+              property_meta,
+            }) => {
+              return (
+                <>
+                  <AllProperty
+                    title={title.rendered}
+                    link={link}
+                    excerpt={excerpt}
+                    featured_media={featured_media}
+                    extra_data={_embedded}
+                    meta={property_meta}
+                  />
+                </>
+              );
+            }
+          )}
+        </SimpleGrid>
+        <Center m="40px">
+          <button  disabledCSS className="buyPropertyNavigation" onClick={() => PreviousPage()}>
+            {" "}
+            <ChevronLeftIcon m={2} w={10} h={10} color="#fab93e" />
+          </button>
+             <button className="buyPropertyNavigationBtn">{NavigateToPageNo}</button>
+             <button className="buyPropertyNavigationBtn">{NavigateToPageNo+1}</button>
+             <button className="buyPropertyNavigationBtn">{NavigateToPageNo+2}</button>
+             <button className="buyPropertyNavigationBtn">{NavigateToPageNo+3}</button>
+          <button className="buyPropertyNavigation" onClick={() => NextPage()}>
+            {" "}
+            <ChevronRightIcon m={2} w={10} h={10} color="#fab93e" />
+          </button>
+        </Center>
+      </Box>
+    </Box>
+  );
+};
 
-  {currentlyAddedItemsFromState?.map(
-    ({ title, link, excerpt, featured_media, _embedded, property_meta }) => {
-      return (<>
-        <AllProperty
-          title={title.rendered}
-          link={link}
-          excerpt={excerpt}
-          featured_media={featured_media}
-          extra_data={_embedded}
-          meta= {property_meta}
-        />
-       
-        </>
-      );
-
-    }
-  )}
-</SimpleGrid>
-    <Center m="40px">    
-      <button className="buyPropertyNavigation" onClick={()=>PrevPage()}>  <ChevronLeftIcon m={2} w={10} h={10} color="#fab93e"/>  </button>
-      <div className="buyPropertyNavigation" onClick={()=>NextPage()}>  <ChevronRightIcon m={2} w={10} h={10} color="#fab93e"/>  </div>
-    </Center>
-</Box>
-</Box>
-)
-
-}
-
-export default connect(AllPropertiesArchive)
+export default connect(AllPropertiesArchive);
--- packages/frontity-chakra-theme/src/components/archive/allProperties.js
+++ packages/frontity-chakra-theme/src/components/archive/allProperties.js
@@ -1,10 +1,10 @@
-import { Box, Flex, Heading, Badge, Image, Icon } from "@chakra-ui/react";
+import { Box, Flex, Heading, Badge, Image, Icon,SimpleGrid } from "@chakra-ui/react";
 import React from "react";
 import { PostImageWithOverlay } from "../featured-post/components";
 import Link from "../link";
 import PostCategories from "../post/post-categories";
 
-const AllProperty = ({title, excerpt, featured_media, link, extra_data, meta }) => {
+const AllProperty = ({ title, featured_media, link, extra_data, meta }) => {
   return (
     <Flex
       direction="column"
@@ -14,14 +14,35 @@ const AllProperty = ({title, excerpt, featured_media, link, extra_data, meta }) 
       shadow="md"
       //   {...rest}
     >
-      {/* Use the frontity settings for featuredPost here */}
-      {featured_media && (
-        <Link link={link}>
-          <Image src={extra_data["wp:featuredmedia"][0].source_url} />
-        </Link>
-      )}
+      <div className="item-of-header">
+        {/* Use the frontity settings for featuredPost here */}
+        {featured_media && (
+          <Link link={link}>
+            <Image
+              className="listing-img"
+              src={extra_data["wp:featuredmedia"][0]?.source_url}
+            />
+          </Link>
+        )}
+        <div className="listing-img-label">
+         {extra_data["wp:term"][1][0]?.name !== undefined && <Badge class="listing-img-label-text">
+            {extra_data["wp:term"][1][0]?.name}
+          </Badge>}
+         {extra_data["wp:term"][3][0]?.name !== undefined &&  <Badge class="listing-img-label-text">
+            {extra_data["wp:term"][3][0]?.name}
+          </Badge>}
+        </div>
 
-      <Flex p="40px" flexGrow="1" direction="column">
+        <div className="listing-price-labelTwo">
+          <ul className="listing-price-labelTwo-ul">
+            <li className="listing-img-price">
+              ${meta?.fave_property_price} {"/"}{" "}
+              {meta?.fave_property_price_postfix}
+            </li>
+          </ul>
+        </div>
+      </div>
+      <Flex pl="19px" flexGrow="1" direction="column">
         {/* <Heading fontSize="2xl" as="h4" textTransform="uppercase">
           <Link link={link}>{title.rendered}</Link>
         </Heading> */}
@@ -31,25 +52,29 @@ const AllProperty = ({title, excerpt, featured_media, link, extra_data, meta }) 
           color="gray.700"
           // dangerouslySetInnerHTML={{ __html: excerpt.rendered }}
         >
-        <Badge>{extra_data["wp:term"][1][0].name}</Badge>
-        <Badge>{extra_data["wp:term"][3][0].name}</Badge>
-          <br />
-          <h6>{title}</h6>
+        <h2 class="listing-item-title">
+        <a href={link}>{title}</a>
+      </h2>
+          
           {meta?.fave_property_address}
           <br />
-          ${meta?.fave_property_price} {"/"} {meta?.fave_property_price_postfix}
-          <br />
-          BedRooms: {meta?.fave_property_bedrooms}{",  "} BathRooms:{" "}
-          {meta?.fave_property_bathrooms}
+          BedRooms: {meta?.fave_property_bedrooms}
+          {",  "} BathRooms: {meta?.fave_property_bathrooms}
           <br />
           Area: {meta?.fave_property_size} {meta?.fave_property_size_prefix}
           <br />
-          <h6>{extra_data["wp:term"][0][0].name}</h6>
+        
+          <div className="row">
+          <div  className="col-8">
+          <h6>{extra_data["wp:term"][0][0]?.name}</h6>  
+          </div>
+          <div  className="col-4">
+          <a className="btn btn-primary listing-btn-details" href={link}> Details  </a>
+          </div>
+          </div>
           
         </Box>
 
-
-        
         {/* <PostCategories
           color="black"
           justify="flex-start"
